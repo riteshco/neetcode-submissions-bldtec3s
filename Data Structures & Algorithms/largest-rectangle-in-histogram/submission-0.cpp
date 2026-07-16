72077815e6dedbe22ae3f7861a2d2eb8dd43937b
Ritesh Garhwal <riteshgadwalv2@gmail.com>
2026-07-16 08:36:14 +0530

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> s;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for(int i=0; i<n; i++) {
+            while(!s.empty() && heights[s.top()] > heights[i]) {
+                int nse = i;
+                int ele = heights[s.top()];
+                s.pop();
+                int pse = s.empty() ? -1 : s.top();
+                maxArea = max(maxArea, ele * (nse - pse - 1)); 
+            }
+            s.push(i);
+        }
+        while(!s.empty()) {
+            int nse = n;
+            int ele = heights[s.top()];
+            s.pop();
+            int pse = s.empty() ? -1 : s.top();
+            maxArea = max(maxArea, ele * (nse - pse - 1));
+        }
+
+        return maxArea;
+    }
+};
