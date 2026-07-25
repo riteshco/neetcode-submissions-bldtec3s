5b9861738774ec87bd0c035421e8f1361514843f
Ritesh Garhwal <riteshgadwalv2@gmail.com>
2026-07-25 17:05:03 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for(int i=1; i<n; i++) {
+            for(int j=i-1; j>=0; j--) {
+                if(dp[j] && j + nums[j] >= i) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
