6a02c8e6ba3b70186085147b6864abf362ef573e
Ritesh Garhwal <riteshgadwalv2@gmail.com>
2026-07-27 23:33:42 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            ans ^= i+1;
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
